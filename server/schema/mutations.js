492483ef521c5d1a69029aae04c14be777918373
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-12-14 11:15:37 +0400

    --

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -10,7 +10,16 @@ const {
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
-       
+        signup: {
+            type: 'UserType',
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString },
+            },
+            resolve(parentValue, args, request) {
+
+            }
+        }
     }
 });
 
